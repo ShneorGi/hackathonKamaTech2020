0b4b946c6263cf7d8639ef78b3e254d2be74ba20
Leah Berkovits <leah.inmed@gmail.com>
2020-12-02 23:44:31 +0200

    leah work

---
+++ packages/client/src/App copy.js
@@ -0,0 +1,30 @@
+import { ReactSVG } from 'react-svg'
+import SvgLogo from './icons/logo.svg'
+import aboutLogo from './icons/aboutLogo.svg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="home_wrapper">
+
+      <ReactSVG
+        beforeInjection={(svg) => {
+          svg.classList.add("welcome_logo_icon");
+        }}
+        src={SvgLogo}
+      />
+
+      <div style={{marginTop:'20px', color: 'white', fontSize: '40px',display:'flex', justifyContent: 'center' }}>מחזירים אהבה</div>
+      <div style={{ color: 'white', fontSize: '40px',display:'flex',justifyContent: 'center' }}>לדור הקודם</div>
+      <ReactSVG
+        beforeInjection={(svg) => {
+          svg.classList.add('logo_icon')
+        }}
+        src={aboutLogo}
+      />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ packages/client/src/Home.tsx
@@ -0,0 +1,69 @@
+// import './App.css';
+// import { ReactSVG } from 'react-svg'
+// import SvgLogo from './icons/logo.svg'
+// import Clozer from './icons/Clozer.svg'
+// import React, { Component } from 'react';
+
+
+// class Home extends React.Component {
+//   render() {
+//     return (
+//       <div className="home_wrapper">
+//         {" "}
+//         <ReactSVG
+//           beforeInjection={(svg) => {
+//             svg.classList.add("logo_icon");
+//           }}
+//           src={SvgLogo}
+//         />
+//         <ReactSVG
+//           beforeInjection={(svg) => {
+//             svg.classList.add("logo_title");
+//           }}
+//           src={Clozer}
+//         />
+//         <div
+//           onClick={() => {}}
+//           style={{
+//             color: "white",
+//             fontSize: "28px",
+//             bottom: "50px",
+//             justifyContent: "center",
+//             marginTop: "380px",
+//           }}
+//         >
+//           {" "}
+//           clozer -ברוכים הבאים ל
+//         </div>
+//         <div
+//           style={{
+//             width: "80%",
+//             height: "70px",
+//             background: "white",
+//             borderRadius: "10px",
+//             marginLeft: "auto",
+//             marginRight: "auto",
+//             marginTop: "15px",
+//             alignItems: "center",
+//             display: "flex",
+//             cursor: "pointer",
+//           }}
+//         >
+//           <div
+//             style={{
+//               color: "#0B204C",
+//               fontSize: "25px",
+//               justifyContent: "center",
+//               display: "flex",
+//               position: "relative",
+//               alignSelf: "center",
+//               marginLeft: "auto",
+//               marginRight: "auto",
+//             }}
+//           >
+//  הבא          </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
